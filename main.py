3b095cd4d7e77bc0e7a96fc12272fa8ad995f41c
l.maslovs <l.maslovs@besk.com>
2018-02-04 11:21:54 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 # coding=utf-8
 
-import json
-import operator
 import os
-import re
-import string
 import sys
-from operator import itemgetter
 
-import html2text as html2text
-import requests
-
-from maxdone import ApiV1
-from maxdone import MaxdoneTxt
+from maxdone import ApiV1, MaxdoneTxt
 
 if __name__ == '__main__':
     api = ApiV1().login(os.environ['USERNAME'], os.environ['PASSWORD'])
     txt = MaxdoneTxt(api)
-    txt.writeTo()
+    txt.writeTo(out=sys.stdout)
